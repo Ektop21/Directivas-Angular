291d89bb3bcc66499479821f3fd92065aa376b75
Hector Fuentes <hectorfuentes2117@gmail.com>
2023-09-29 12:38:41 -0600

    ngif y ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  title = 'Registro de Usuarios';
+  mensaje = "";
+  registrado = false;
+  nombre: string = "";
+  apellido: string = "";
+  entradas: any[];
+
+
+  constructor() {
+
+    this.entradas = [
+
+      { titulo: "Python cada dia mas presente" },
+      { titulo: "Java presente desde hacemas de 20 years" },
+      { titulo: "Kotlin potencia para tus apps" },
+      { titulo: "JavaScript cada vez mas funciional" },
+      { titulo: "Donde quedo pascal?" },
+    ]
+
+
+  }
+
+
+
+  registrarUsuario() {
+
+    this.registrado = true;
+
+    this.mensaje = "Usuario Registrado con Exito "
+
+  }
 }
